e2a91e15e5015434f26a2c470f167a96c764b725
Randie <srandall1213@gmail.com>
2022-07-03 23:17:03 -0600

    figured out transitions. need conditions to transition

--- README.md
+++ README.md
@@ -1,27 +1,14 @@
 # Code-Quiz
- <div class="questionbox"></div>
-            <h1 class="question"></h1>
-            <ol>
-                <li class="answer1"></li>
-                <li class="answer2"></li>
-                <li class="answer3"></li>
-                <li class="answer4"></li>
-            </ol>
-        </div>
 
 
+<div class="allDone">
+    <h1>All done!</h1>
+    <p>Your final score is:<span id="score"></span></p>
+    <p>Enter initials: <input type="text" id="initials"></input><button id="submit">Submit</button></p>
+</div>
 
-
-
-<div class="alldone">
-            <h1>All done!</h1>
-            <p>Your final score is:<span id="score"></span></p>
-            <p>Enter initials: <input type="text" id="initials"></input><button id="submit">Submit</button></p>
-        </div>
-
-        <div class="highscores">
-            <h1>High Scores</h1>
-            
-            <!-- div of 1. "ititials" + "-" + "score" -->
-            <button id="backbtn">Go Back</button><button id="clearscores">Clear High Scores</button>
-        </div>
+<div class="highScores">
+    <h1>High Scores</h1> 
+    <div id="highestScore"></div> <!-- div of 1. "ititials" + "-" + "score" -->
+    <button id="backBtn">Go Back</button><button id="clearScores">Clear High Scores</button>
+</div>
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,10 +4,22 @@
 //all questions answered or timer reaches 0.. game over
 //save initials and score 
 
-var timerEl = document.getElementById("#timer");
-var startbtn = document.getElementById("#startbtn");
+var startBtn = document.querySelector("#startBtn");
+var timerEl = document.querySelector("#timer");
+var introTextEl = document.querySelector("#introText");
 
-function countdown() {
+var questionBox1 = document.querySelector("#questionBox1");
+var questionBox2 = document.querySelector("#questionBox2");
+var questionBox3 = document.querySelector("#questionBox3");
+var questionBox4 = document.querySelector("#questionBox4");
+var questionBox5 = document.querySelector("#questionBox5");
+
+//Click start to start timer & start quiz
+startBtn.addEventListener("click", countDown);
+startBtn.addEventListener("click", startQuiz);
+
+//Timer
+function countDown() {
     var timeLeft = 60;
     var timeInterval = setInterval(function () {
         timeLeft--;
@@ -19,8 +31,50 @@ function countdown() {
     }, 1000);
 }
 
-startbtn.addEventListener("click", function (){
-    countdown();
-});
+//Transition to Question 1 
+function startQuiz() {
+    introTextEl.setAttribute("style", "display: none");
+    questionBox1.setAttribute("style", "display: flex");
+    showQuestion1()
+}
+
+//Question 1 
+function showQuestion1 () {
+    questionBox1.children[0].textContent = "My first question";
+    questionBox1.children[1].children[0].textContent = "what's up"
+    questionBox1.children[1].children[1].textContent = "how are you";
+    questionBox1.children[1].children[2].textContent = "see ya";
+    questionBox1.children[1].children[3].textContent = "goodbye";
+    if (answerButtons1.addEventListener("click", transition1)) { //CHANGE ANSWERBUTTONS1 TO THE CORRECT ANSWER BUTTON
+        console.log("you win")
+    } else {
+        console.log("time off clock")
+    }
+
+}
+
+//Transition to Question 2
+function transition1() {
+    questionBox1.setAttribute("style", "display: none")
+    questionBox2.setAttribute("style", "display: flex")
+    showQuestion2()
+}
+
+//Question 2
+function showQuestion2 () {
+    questionBox2.children[0].textContent = "My second question";
+    questionBox2.children[1].children[0].textContent = "good morning";
+    questionBox2.children[1].children[1].textContent = "good afternoon";
+    questionBox2.children[1].children[2].textContent = "good day";
+    questionBox2.children[1].children[3].textContent = "good night";
+}
+
+function selectAnswer () {
+
+
+}
+
+
+
 
 
