a9db665642a112732b9aed6f7c3a13315c7326f8
Randie <srandall1213@gmail.com>
2022-07-08 10:13:21 -0600

    added for loops for wrong answers

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Code-Quiz
 
+//WHY ARE QUESTIONS JUMPING AROUND
+
 //Time off timer is wonky with incorrect answers
 
 //Write in questions
 
 //Figure out how to make it not record points for wrong answers when time runs out
-
-//When Back Button is pushed it needs to Reset Score or is the clear scores button sufficient for that purpose?
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,9 +8,14 @@ var correct = document.querySelector('#correct')
 var wrong0 = document.querySelector('#wrong0')
 var wrong1 = document.querySelector('#wrong1')
 var wrong2 = document.querySelector('#wrong2')
-var questionsArr = ["First", "Second", "Third", "Fourth", "Fifth"];
-var correctArr = ["First Correct", "Second Correct", "Third Correct", "Fourth Correct", "Fifth Correct"];
-var wrongArr = ["Wrong 0", "Wrong 1", "Wrong 2"];
+var questionsArr = ["First What is the term called that allows something to happen with a click? ",
+                    "Second What do you need to do after an element is created in JavaScript?", 
+                    "Third Which function allows you to set a timer?", 
+                    "Fourth Which property allows you to save values in a web browser until with no expiration?", 
+                    "Fifth What kind of loop runs a block of code as long as a specified condition is true?"];
+var correctArr = ["addEventListener", "append", "setInterval()", "localStorage", "for loop"];
+var wrongArr = ["while loop", "get loop", "bubbling", "sessionStorage", "setTime()", ".createElement", ".setAttribute", "index loop", "event.preventDefault()", "timer()", "browswerStorage", "repeat loop",
+"store", "addEvent", "concatenate"];
 
 var enterInitialsBox = document.querySelector("#enterInitialsBox");
 var scoreEl = document.querySelector("#score");
@@ -29,6 +34,7 @@ startBtn.addEventListener("click", startQuiz);
 //Starts Quiz
 function startQuiz() {
 var quizScore = 0;
+
 
     //Timer
     function countDown() {
@@ -55,7 +61,7 @@ var quizScore = 0;
 
         }, 1000);
     }
-    countDown();
+   countDown();
 
     //Show Question 1
     function question1() {
@@ -64,79 +70,90 @@ var quizScore = 0;
         
         question.textContent = questionsArr[0];
         correct.textContent = correctArr[0];
-        wrong0.textContent = wrongArr[0];
-        wrong1.textContent = wrongArr[1];
-        wrong2.textContent = wrongArr[2];
 
+        for (i=0; i < wrongArr.length; i++) {
+            wrong0.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+            wrong1.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+            wrong2.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+        }
+        
         correct.addEventListener("click", question2, quizScore++); //Select Correct Answer 
         wrong0.addEventListener("click", question2, quizScore--); //Select Wrong Answer 
         wrong1.addEventListener("click", question2, quizScore--); //Select Wrong Answer 
-        wrong2.addEventListener("click", question2, quizScore--); //Select Wrong Answer 
+        wrong2.addEventListener("click", question2, quizScore--); //Select Wrong Answer   
     }
     question1();
     
-    //Show Question 2
+    // Show Question 2
     function question2() {
         question.textContent = questionsArr[1];
         correct.textContent = correctArr[1];
-        wrong0.textContent = wrongArr[0]
-        wrong1.textContent = wrongArr[1];
-        wrong2.textContent = wrongArr[2];
+
+        for (i=0; i < wrongArr.length; i++) {
+            wrong0.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+            wrong1.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+            wrong2.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+        }
 
         correct.addEventListener("click", question3, quizScore++); //Select Correct Answer
         wrong0.addEventListener("click", question3, quizScore--); //Select Wrong Answer 
         wrong1.addEventListener("click", question3, quizScore--); //Select Wrong Answer 
         wrong2.addEventListener("click", question3, quizScore--); //Select Wrong Answer
-    }
-    question2();
+    };
+    question2() 
     
     //Show Question 3
     function question3() {
         question.textContent = questionsArr[2];
         correct.textContent = correctArr[2];
-        wrong0.textContent = wrongArr[0]
-        wrong1.textContent = wrongArr[1];
-        wrong2.textContent = wrongArr[2];
+        
+        for (i=0; i < wrongArr.length; i++) {
+            wrong0.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+            wrong1.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+            wrong2.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+        }
 
         correct.addEventListener("click", question4, quizScore++); //Select Correct Answer
         wrong0.addEventListener("click", question4, quizScore--); //Select Wrong Answer 
         wrong1.addEventListener("click", question4, quizScore--); //Select Wrong Answer 
         wrong2.addEventListener("click", question4, quizScore--); //Select Wrong Answer
-    }
-    question3();
+    };
 
     //Show Question 4
     function question4() {
         question.textContent = questionsArr[3];
         correct.textContent = correctArr[3];
-        wrong0.textContent = wrongArr[0]
-        wrong1.textContent = wrongArr[1];
-        wrong2.textContent = wrongArr[2];
+
+        for (i=0; i < wrongArr.length; i++) {
+            wrong0.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+            wrong1.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+            wrong2.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+        }
 
         correct.addEventListener("click", question5, quizScore++); //Select Correct Answer
         wrong0.addEventListener("click", question5, quizScore--); //Select Wrong Answer 
         wrong1.addEventListener("click", question5, quizScore--); //Select Wrong Answer 
         wrong2.addEventListener("click", question5, quizScore--); //Select Wrong Answer
-    }
-    question4();
+    };
 
     //Show Question 5
     function question5() {
         question.textContent = questionsArr[4];
         correct.textContent = correctArr[4];
-        wrong0.textContent = wrongArr[0]
-        wrong1.textContent = wrongArr[1];
-        wrong2.textContent = wrongArr[2];
+
+        for (i=0; i < wrongArr.length; i++) {
+            wrong0.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+            wrong1.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+            wrong2.textContent = wrongArr[Math.floor(Math.random () * wrongArr.length)];
+        }
 
         correct.addEventListener("click", enterInitials, quizScore++); //Select Correct Answer
         wrong0.addEventListener("click", enterInitials, quizScore--); //Select Wrong Answer 
         wrong1.addEventListener("click", enterInitials, quizScore--); //Select Wrong Answer 
         wrong2.addEventListener("click", enterInitials, quizScore--); //Select Wrong Answer
-        correct.addEventListener("click", enterInitials, quizScore++); //Select Correct Answer 5
-    }
-    question5();
-
-    //Show Score & Enter Initials
+    };
+    
+    //Show Score & Enter Initials // STOP COUNTDOWN HERE!!!!!
     function enterInitials () {
         questionBox.setAttribute("style", "display: none");
         enterInitialsBox.setAttribute("style", "display: flex");
@@ -161,7 +178,6 @@ var quizScore = 0;
     showYourScore();
     });
 
-
     //Show Your Stored Score with Initials
     function showYourScore () {
         enterInitialsBox.setAttribute("style", "display: none");
@@ -171,8 +187,11 @@ var quizScore = 0;
 }
 
 //Back Button To Intro Page (keeps score)
-backBtn.addEventListener("click", startQuiz);
-
+backBtn.addEventListener("click", function () {
+    yourScoreBox.setAttribute("style", "display: none");
+    introBox.setAttribute("style", "display: flex");
+});
+    
 //Clears All Stored Scores
 clearBtn.addEventListener("click", clearScores);
 
